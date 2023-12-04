513387be75b50645850d34c9e26212b56dbd23ef
emily shea <emilyshea947@gmail.com>
2023-12-03 19:50:27 -0500

    Edited Code 1

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,49 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
+
+  $(function () {
+  
+    function updateBlockStatus() {
+      var currentHour = dayjs().hour();
+  
+      $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        if (blockHour < currentHour) {
+          $(this).removeClass("present future").addClass("past");
+        } else if (blockHour === currentHour) {
+          $(this).removeClass("past future").addClass("present");
+        } else {
+          $(this).removeClass("past present").addClass("future");
+        }
+      });
+    }
+  
+    $(".container-fluid").on("click", ".saveBtn", function () {
+      var timeBlock = $(this).parent();
+      var eventText = timeBlock.find(".description").val();
+      var blockId = timeBlock.attr("id");
+  
+      localStorage.setItem(blockId, eventText);
+    });
+  
+    function loadSavedEvents() {
+      $(".time-block").each(function () {
+        var blockId = $(this).attr("id");
+        var savedEvent = localStorage.getItem(blockId);
+  
+        if (savedEvent !== null) {
+          $(this).find(".description").val(savedEvent);
+        }
+      });
+    }
+  
+    function displayCurrentDate() {
+      var currentDay = dayjs().format("dddd, MMMM D");
+      $("#currentDay").text("Today is " + currentDay);
+    }
+  
+    updateBlockStatus();
+    loadSavedEvents();
+    displayCurrentDate();
+  });
