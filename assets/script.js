ad767ad912f423362fdf834598833342ee67491b
emily shea <emilyshea947@gmail.com>
2023-12-03 20:43:48 -0500

    Updated Java

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,8 +20,52 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
-
+  
+//$(function () {
+  
+   // function updateBlockStatus() {
+      //var currentHour = dayjs().hour();
+  
+      //$(".time-block").each(function () {
+       // var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        //if (blockHour < currentHour) {
+        //  $(this).removeClass("present future").addClass("past");
+        //} else if (blockHour === currentHour) {
+         // $(this).removeClass("past future").addClass("present");
+        //} else {
+         // $(this).removeClass("past present").addClass("future");
+        //}
+    //  });
+   // }
+  
+   // $(".container-fluid").on("click", ".saveBtn", function () {
+      //var timeBlock = $(this).parent();
+      //var eventText = timeBlock.find(".description").val();
+      //var blockId = timeBlock.attr("id");
+  
+    //  localStorage.setItem(blockId, eventText);
+    //});
+  
+   // function loadSavedEvents() {
+      //$(".time-block").each(function () {
+       // var blockId = $(this).attr("id");
+        //var savedEvent = localStorage.getItem(blockId);
+  
+       // if (savedEvent !== null) {
+        //  $(this).find(".description").val(savedEvent);
+      //  }
+      //});
+    //}
+  
+   // function displayCurrentDate() {
+   //   var currentDay = dayjs().format("dddd, MMMM D");
+   //   $("#currentDay").text("Today is " + currentDay);
+   // }
+  
+   // updateBlockStatus();
+   // loadSavedEvents();
+  //  displayCurrentDate();
+ // });
   $(function () {
   
     function updateBlockStatus() {
